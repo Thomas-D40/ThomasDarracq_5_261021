5c6d6ac15500a90c6a3b50888141406fe975d34a
Thomas DARRACQ <alexander.darracq@gmail.com>
2021-10-27 15:29:20 +0200

    First commit + adding product on index

---
+++ front/js/script.js
@@ -0,0 +1,29 @@
+const items = document.getElementById("items");
+
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (kanaps) {
+    for (var i = 0; i < kanaps.length; i++) {
+      var kanapLink = document.createElement("a");
+      var kanapArticle = document.createElement("article");
+      var kanapPicture = document.createElement("img");
+      var kanapTitle = document.createElement("h3");
+      var kanapDescription = document.createElement("p");
+
+      kanapLink.href = "./product.html?" + kanaps[i]._id;
+      kanapPicture.src = kanaps[i].imageUrl;
+      kanapTitle.innerHTML = kanaps[i].name;
+      kanapDescription.innerHTML = kanaps[i].description;
+
+      kanapLink.appendChild(kanapArticle);
+      kanapArticle.appendChild(kanapPicture);
+      kanapArticle.appendChild(kanapTitle);
+      kanapArticle.appendChild(kanapDescription);
+
+      items.appendChild(kanapLink);
+    }
+  });

5527a779c2536fefe7609578a41d4a5cfa4a7899
Thomas DARRACQ <alexander.darracq@gmail.com>
2021-11-04 18:02:34 +0100

    Création array, voir pour tri pour panier

--- front/js/product.js
+++ front/js/product.js
@@ -12,6 +12,8 @@ let url = new URL(window.location.href);
 let id = url.searchParams.get("id");
 console.log(id);
 
+// Affichage produit
+
 fetch("http://localhost:3000/api/products")
   .then(function (res) {
     if (res.ok) {
@@ -46,15 +48,85 @@ fetch("http://localhost:3000/api/products")
     }
   });
 
-addToCart.addEventListener("click", function () {
-  var objJson = {
+// Création du panier
+
+var productArray = [];
+
+//Ajout au panier
+
+addToCart.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Vérification que la quantité est bien un nombre
+  if (Number.isInteger(parseInt(quantity.value)) === false) {
+    alert("Merci de renseigner un nombre de produits");
+    return;
+  }
+  console.log("Item added ");
+
+  var productJSON = {
     name: title.innerHTML,
     color: colors.options[colors.selectedIndex].text,
     quantity: quantity.value,
   };
-  let objLinea = JSON.stringify(objJson);
-  localStorage.setItem("obj", objLinea);
-  console.log(objLinea);
-  console.log(localStorage.length);
-  console.log(objJson);
+  console.log(productJSON);
+  console.log(productJSON.color);
+
+  ///////////////// STORAGE
+
+  if (localStorage.getObj("product") !== null) {
+    //// Recherche produit identique
+
+    // if (
+    //   productArray.indexOf(productJSON.color) !== -1 &&
+    //   productArray.indexOf(productJSON.name) !== -1
+    // ) {
+    //   productArray = localStorage.getObj("product");
+    //   productArray.push(productJSON.quantity);
+    //   localStorage.setObj("product", productArray);
+    // } else {
+    productArray = localStorage.getObj("product");
+    productArray.push(productJSON);
+    localStorage.setObj("product", productArray);
+  } else {
+    productArray.push(productJSON);
+    localStorage.setObj("product", productArray);
+  }
+  console.log(productArray);
 });
+
+Storage.prototype.setObj = function (key, value) {
+  this.setItem(key, JSON.stringify(value));
+};
+
+Storage.prototype.getObj = function (key) {
+  var value = this.getItem(key);
+  return value && JSON.parse(value);
+};
+
+// Vérification doublon objet
+
+//All values stored in localStorage are strings.
+//Grab our itemsline string from localStorage.
+var stringFromLocalStorage = window.localStorage.getItem("itemsline");
+
+//Then parse that string into an actual value.
+var parsedValueFromString = JSON.parse(stringFromLocalStorage);
+
+//If that value is null (meaning that we've never saved anything to that spot in localStorage before), use an empty array as our array. Otherwise, just stick with the value we've just parsed out.
+var array = parsedValueFromString || [];
+
+//Here's the value we want to add
+var value = "some value";
+
+//If our parsed/empty array doesn't already have this value in it...
+if (array.indexOf(value) == -1) {
+  //add the value to the array
+  array.push(value);
+
+  //turn the array WITH THE NEW VALUE IN IT into a string to prepare it to be stored in localStorage
+  var stringRepresentingArray = JSON.stringify(array);
+
+  //and store it in localStorage as "itemsline"
+  window.localStorage.setItem("itemsline", stringRepresentingArray);
+}
